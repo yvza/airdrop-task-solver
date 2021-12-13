96aeb058efab462d6bdf5abbb337b9572f8de409
yuza <akhmadyuza@gmail.com>
2021-12-13 10:15:37 +0700

    tailwind configuration

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .vue(3)
     .postCss('resources/css/app.css', 'public/css', [
-        //
+        require("tailwindcss")
     ])
     .version();
