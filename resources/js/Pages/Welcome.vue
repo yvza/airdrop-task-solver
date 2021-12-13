2bd8bfe04e7f513353d22f5b2a5cb7a0ea245391
yuza <akhmadyuza@gmail.com>
2021-12-13 21:55:54 +0700

    aside base content

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,5 +1,33 @@
 <template>
-  <h1>Halo gayss</h1>
+  <section class="container mx-auto flex h-screen">
+    <aside class="w-1/6 p-6 origin-bg-color text-slate-400">
+      <div class="">
+        <img src="../../img/airdrop_task_solver.png" alt="Airdrop Task Solver">
+      </div>
+      <div class="w-5/6 mt-12">
+        <ul>
+          <li class="my-7">
+            <i class="fas fa-plus-square"></i>
+            &nbsp;
+            <a href="">Garapan Nyar</a>
+          </li>
+          <li class="my-7">
+            <i class="fas fa-list"></i>
+            &nbsp;
+            <a href="">List Garapan</a>
+          </li>
+          <li class="my-7">
+            <i class="fas fa-file-alt"></i>
+            &nbsp;
+            <a href="">List Kata-kata</a>
+          </li>
+        </ul>
+      </div>
+    </aside>
+    <article>
+      Artikel
+    </article>
+  </section>
 </template>
 <script>
 export default {
@@ -7,5 +35,7 @@ export default {
 }
 </script>
 <style>
-  
+  .origin-bg-color {
+    background: #0F2133;
+  }
 </style>
