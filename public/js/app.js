9f4e9e1f2b8271060272daba761710e0759b4037
yuza <akhmadyuza@gmail.com>
2021-12-13 22:22:58 +0700

    generated from laravel

--- public/js/app.js
+++ public/js/app.js
@@ -20122,7 +20122,7 @@ var _hoisted_1 = {
   "class": "container mx-auto flex h-screen"
 };
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<aside class=\"w-1/6 p-6 origin-bg-color text-slate-400\"><div class=\"\"><img src=\"" + _img_airdrop_task_solver_png__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Airdrop Task Solver\"></div><div class=\"w-5/6 mt-12\"><ul><li class=\"my-7\"><i class=\"fas fa-plus-square\"></i>   <a href=\"\">Garapan Nyar</a></li><li class=\"my-7\"><i class=\"fas fa-list\"></i>   <a href=\"\">List Garapan</a></li><li class=\"my-7\"><i class=\"fas fa-file-alt\"></i>   <a href=\"\">List Kata-kata</a></li></ul></div></aside><article> Artikel </article>", 2);
+var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<aside class=\"w-1/6 p-6 origin-bg-color text-slate-400\"><div class=\"\"><img src=\"" + _img_airdrop_task_solver_png__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Airdrop Task Solver\"></div><div class=\"w-5/6 mt-12\"><ul><li class=\"p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200\"><i class=\"fas fa-plus-square\"></i>   <a href=\"\">Garapan Nyar</a></li><li class=\"p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200\"><i class=\"fas fa-list\"></i>   <a href=\"\">List Garapan</a></li><li class=\"p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200\"><i class=\"fas fa-file-alt\"></i>   <a href=\"\">List Kata-kata</a></li></ul></div></aside><article> Artikel </article>", 2);
 
 var _hoisted_4 = [_hoisted_2];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
