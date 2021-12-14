07e4cb1a7a62485224b073d0ae678590430f56ba
yuza <akhmadyuza@gmail.com>
2021-12-14 09:04:03 +0700

    aside positioning

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,19 +4,19 @@
       <div class="">
         <img src="../../img/airdrop_task_solver.png" alt="Airdrop Task Solver">
       </div>
-      <div class="w-5/6 mt-12">
-        <ul>
-          <li class="p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200">
+      <div class="mt-12">
+        <ul class="w-52 flex flex-col">
+          <li class="p-5 my-3 w-full hover:bg-cyan-900 hover:rounded hover:text-slate-200">
             <i class="fas fa-plus-square"></i>
             &nbsp;
             <a href="">Garapan Nyar</a>
           </li>
-          <li class="p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200">
+          <li class="p-5 my-3 w-full hover:bg-cyan-900 hover:rounded hover:text-slate-200">
             <i class="fas fa-list"></i>
             &nbsp;
             <a href="">List Garapan</a>
           </li>
-          <li class="p-5 my-3 w-52 hover:bg-cyan-900 hover:rounded hover:text-slate-200">
+          <li class="p-5 my-3 w-full hover:bg-cyan-900 hover:rounded hover:text-slate-200">
             <i class="fas fa-file-alt"></i>
             &nbsp;
             <a href="">List Kata-kata</a>
@@ -24,7 +24,7 @@
         </ul>
       </div>
     </aside>
-    <article>
+    <article class="w-5/6">
       Artikel
     </article>
   </section>
