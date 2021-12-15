b5a1879b864730c27c6163ea48c9ebefca73933c
yuza <akhmadyuza@gmail.com>
2021-12-15 11:15:26 +0700

    generated from laravel

--- public/js/app.js
+++ public/js/app.js
@@ -20099,7 +20099,25 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({});
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  name: 'Welcome',
+  data: function data() {
+    return {
+      isDark: false
+    };
+  },
+  methods: {
+    modeToggled: function modeToggled() {
+      this.isDark = !this.isDark;
+
+      if (this.isDark) {
+        document.querySelector('html').classList.add('dark');
+      } else {
+        document.querySelector('html').classList.remove('dark');
+      }
+    }
+  }
+});
 
 /***/ }),
 
@@ -20121,12 +20139,45 @@ __webpack_require__.r(__webpack_exports__);
 var _hoisted_1 = {
   "class": "flex"
 };
+var _hoisted_2 = {
+  "class": "w-1/6 p-6 h-screen origin-bg-color text-slate-400 flex flex-col"
+};
 
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<aside class=\"w-1/6 p-6 h-screen origin-bg-color text-slate-400\"><div class=\"\"><img src=\"" + _img_airdrop_task_solver_png__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Airdrop Task Solver\"></div><div class=\"mt-12\"><ul class=\"w-52 flex flex-col\"><li class=\"p-5 my-1 w-full shadow-md bg-cyan-900 rounded text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-plus-square\"></i><a class=\"ml-3\" href=\"\">Garapan Nyar</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-list\"></i><a class=\"ml-3\" href=\"\">List Garapan</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-users-cog\"></i><a class=\"ml-3\" href=\"\">Ngatur Akun</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-file-alt\"></i><a class=\"ml-3\" href=\"\">Ngatur Kata-Kata</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"far fa-lightbulb\"></i><a class=\"ml-3\" href=\"\">Basis Kawruh</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-info-circle\"></i><a class=\"ml-3\" href=\"\">Kridit</a></li></ul></div></aside><article class=\"w-5/6 flex flex-col\"><div class=\"px-10 pt-10 shadow-lg\"><div class=\"head-content\"><h1 class=\"text-2xl font-bold font-sans\">Garapan Nyar</h1><h6 class=\"text-sm text-slate-400 font-sans\">Nok kene nggone gawe nambah garapan</h6></div><div class=\"head-menu pt-10\"><ul class=\"flex\"><li class=\"py-3 mr-3 border-b-4 border-blue-500 hover:cursor-pointer\">Langkah 1</li><li class=\"py-3 mx-3 hover:border-b-4 hover:border-blue-500 hover:cursor-pointer\">Langkah 2</li><li class=\"py-3 mx-3 hover:border-b-4 hover:border-blue-500 hover:cursor-pointer\">Langkah 3</li></ul></div></div><div class=\"content grow bg-slate-100 p-10 overflow-x-scroll\"> Konten tes <div class=\"form-control\"><label class=\"label\"><span class=\"label-text\">Username</span></label><input type=\"text\" placeholder=\"username\" class=\"input input-bordered\"></div></div></article>", 2);
+var _hoisted_3 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"\"><img src=\"" + _img_airdrop_task_solver_png__WEBPACK_IMPORTED_MODULE_1__["default"] + "\" alt=\"Airdrop Task Solver\"></div><div class=\"mt-12\"><ul class=\"w-52 flex flex-col\"><li class=\"p-5 my-1 w-full shadow-md bg-cyan-900 rounded text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-plus-square\"></i><a class=\"ml-3\" href=\"\">Garapan Nyar</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-list\"></i><a class=\"ml-3\" href=\"\">List Garapan</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-users-cog\"></i><a class=\"ml-3\" href=\"\">Ngatur Akun</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-file-alt\"></i><a class=\"ml-3\" href=\"\">Ngatur Kata-Kata</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"far fa-lightbulb\"></i><a class=\"ml-3\" href=\"\">Basis Kawruh</a></li><li class=\"p-5 my-1 w-full hover:shadow-md hover:bg-cyan-900 hover:rounded hover:text-slate-200 hover:cursor-pointer\"><i class=\"fas fa-info-circle\"></i><a class=\"ml-3\" href=\"\">Kridit</a></li></ul></div>", 2);
 
-var _hoisted_4 = [_hoisted_2];
+var _hoisted_5 = {
+  "class": "grow flex items-end justify-center"
+};
+var _hoisted_6 = {
+  "class": "flex items-center"
+};
+
+var _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("i", {
+  "class": "fas fa-sun"
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_8 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" ");
+
+var _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createTextVNode)(" ");
+
+var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("i", {
+  "class": "fas fa-moon"
+}, null, -1
+/* HOISTED */
+);
+
+var _hoisted_11 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<article class=\"w-5/6 flex flex-col\"><div class=\"px-10 pt-10 shadow-lg\"><div class=\"head-content\"><h1 class=\"text-2xl font-bold font-sans\">Garapan Nyar</h1><h6 class=\"text-sm text-slate-400 font-sans dark:text-slate-900\">Nok kene nggone gawe nambah garapan</h6></div><div class=\"head-menu pt-10\"><ul class=\"flex\"><li class=\"py-3 mr-3 border-b-4 border-blue-500 hover:cursor-pointer\">Langkah 1</li><li class=\"py-3 mx-3 hover:border-b-4 hover:border-blue-500 hover:cursor-pointer\">Langkah 2</li><li class=\"py-3 mx-3 hover:border-b-4 hover:border-blue-500 hover:cursor-pointer\">Langkah 3</li></ul></div></div><div class=\"content grow bg-slate-100 p-10 overflow-x-scroll\"> Konten <div class=\"form-control\"><label class=\"label\"><span class=\"label-text\">Username</span></label><input type=\"text\" placeholder=\"username\" class=\"input input-bordered\"></div></div></article>", 1);
+
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("section", _hoisted_1, _hoisted_4);
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("section", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("aside", _hoisted_2, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_5, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_6, [_hoisted_7, _hoisted_8, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+    type: "checkbox",
+    "class": "toggle",
+    onClick: _cache[0] || (_cache[0] = function ($event) {
+      return $options.modeToggled();
+    })
+  }), _hoisted_9, _hoisted_10])])]), _hoisted_11]);
 }
 
 /***/ }),
