f23ec5527e1324a89c5d46d3b76fa3995c509f4a
yuza <akhmadyuza@gmail.com>
2021-12-16 10:58:42 +0700

    task results migration

---
+++ database/migrations/2021_12_16_033344_create_task_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task_results', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('tasks_id');
+            $table->integer('type');
+            $table->integer('status');
+            $table->string('messages');
+            $table->timestamps();
+            
+            $table->foreign('tasks_id')->references('id')->on('tasks');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task_results');
+    }
+}
