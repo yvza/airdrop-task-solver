97184b2a2dc6d10c42abf2d63ec9873bb9a53bf9
yuza <akhmadyuza@gmail.com>
2021-12-21 15:15:27 +0700

    SideBarLink component

---
+++ resources/js/Shared/SideBarLink.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <Link 
+    class="p-5 my-1 w-full"
+    :class="{'shadow-md bg-cyan-900 rounded text-slate-200 hover:cursor-pointer': active}"
+  >
+    <slot />
+  </Link>
+</template>
+<script>
+import { Link } from '@inertiajs/inertia-vue3'
+
+export default {
+  components: { Link },
+  props: {
+    active: Boolean
+  }
+}
+</script>
+<style lang="">
+  
+</style>
