39b6ff487509cd2eae010f917bd780ad4282d561
yuza <akhmadyuza@gmail.com>
2021-12-21 15:36:35 +0700

    indent for better code read

--- resources/js/Shared/SideBar.vue
+++ resources/js/Shared/SideBar.vue
@@ -5,12 +5,42 @@
     </div>
     <div class="mt-12">
       <ul class="w-52 flex flex-col">
-        <SideBarLink href="/" :active="$page.component === 'GarapanNyar'"><i class="fas fa-plus-square mr-3"></i> Garapan Nyar</SideBarLink>
-        <SideBarLink href="/list-garapan" :active="$page.component === 'ListGarapan'"><i class="fas fa-list mr-3"></i> List Garapan</SideBarLink>
-        <SideBarLink href="/ngatur-akun" :active="$page.component === 'NgaturAkun'"><i class="fas fa-users-cog mr-3"></i> Ngatur Akun</SideBarLink>
-        <SideBarLink href="/ngatur-kata-kata" :active="$page.component === 'NgaturKataKata'"><i class="fas fa-file-alt mr-3"></i> Ngatur Kata Kata</SideBarLink>
-        <SideBarLink href="/basis-kawruh" :active="$page.component === 'BasisKawruh'"><i class="far fa-lightbulb mr-3"></i> Basis Kawruh</SideBarLink>
-        <SideBarLink href="/kridit" :active="$page.component === 'Kridit'"><i class="fas fa-info-circle mr-3"></i> Garapan Nyar</SideBarLink>
+        <SideBarLink 
+          href="/" 
+          :active="$page.component === 'GarapanNyar'"
+        >
+          <i class="fas fa-plus-square mr-3"></i> Garapan Nyar
+        </SideBarLink>
+        <SideBarLink 
+          href="/list-garapan" 
+          :active="$page.component === 'ListGarapan'"
+        >
+          <i class="fas fa-list mr-3"></i> List Garapan
+        </SideBarLink>
+        <SideBarLink 
+          href="/ngatur-akun" 
+          :active="$page.component === 'NgaturAkun'"
+        >
+          <i class="fas fa-users-cog mr-3"></i> Ngatur Akun
+        </SideBarLink>
+        <SideBarLink 
+          href="/ngatur-kata-kata" 
+          :active="$page.component === 'NgaturKataKata'"
+        >
+          <i class="fas fa-file-alt mr-3"></i> Ngatur Kata Kata
+        </SideBarLink>
+        <SideBarLink 
+          href="/basis-kawruh" 
+          :active="$page.component === 'BasisKawruh'"
+        >
+          <i class="far fa-lightbulb mr-3"></i> Basis Kawruh
+        </SideBarLink>
+        <SideBarLink 
+          href="/kridit" 
+          :active="$page.component === 'Kridit'"
+        >
+          <i class="fas fa-info-circle mr-3"></i> Garapan Nyar
+        </SideBarLink>
       </ul>
     </div>
     <div class="grow flex items-end justify-center">
