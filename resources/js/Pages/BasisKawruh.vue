a25694a96d896cb30c60417f6c3ab580c2e478a8
yuza <akhmadyuza@gmail.com>
2021-12-21 15:40:02 +0700

    core to change

--- resources/js/Pages/BasisKawruh.vue
+++ resources/js/Pages/BasisKawruh.vue
@@ -1,11 +1,13 @@
 <template lang="">
-  <div>
-    basis kawruh
-  </div>
+  <Layout>
+    Basis Kawruh
+  </Layout>
 </template>
 <script>
+import Layout from '../Shared/Layout.vue'
+
 export default {
-  
+  components: { Layout }
 }
 </script>
 <style lang="">
