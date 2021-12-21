0bc1b21002eee04b325fec72a7c6efeb32ddfb9f
yuza <akhmadyuza@gmail.com>
2021-12-21 15:14:28 +0700

    sidebar using component added

--- resources/js/Pages/ListGarapan.vue
+++ resources/js/Pages/ListGarapan.vue
@@ -1,11 +1,26 @@
 <template lang="">
-  <div>
+  <div class="flex">
+    <aside class="w-1/6 p-6 h-screen origin-bg-color text-slate-400 flex flex-col">
+      <div class="">
+        <img src="../../img/airdrop_task_solver.png" alt="Airdrop Task Solver">
+      </div>
+      <div class="mt-12">
+        <SideBar />
+      </div>
+      <div class="grow flex items-end justify-center">
+        <div class="flex items-center">
+          <i class="fas fa-sun"></i>&nbsp;<input type="checkbox" class="toggle" @click="modeToggled()">&nbsp;<i class="fas fa-moon"></i>
+        </div>
+      </div>
+    </aside>
     list garapan
   </div>
 </template>
 <script>
+import SideBar from "../Shared/SideBar.vue"
+
 export default {
-  
+  components: { SideBar }
 }
 </script>
 <style lang="">
