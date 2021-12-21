d6c6abb0c74b7110de919bef0fa643f944b1ddb1
yuza <akhmadyuza@gmail.com>
2021-12-21 15:33:52 +0700

    code refactor

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <section class="flex">
+    <SideBar />
+    <slot />
+  </section>
+</template>
+<script>
+import SideBar from "./SideBar.vue"
+
+export default {
+  components: { SideBar }
+}
+</script>
+<style>
+  .origin-bg-color {
+    background: #0F2133;
+  }
+</style>
