a5616c04540f5e55215510ef3d1a2e7de80dabc5
yuza <akhmadyuza@gmail.com>
2021-12-27 15:11:15 +0700

    adding dynamic app title and meta tag component

---
+++ resources/js/Shared/AppHead.vue
@@ -0,0 +1,18 @@
+<template lang="">
+  <Head :title="title ? `${title}` : 'ATS'">
+    <slot />
+  </Head>
+</template>
+<script>
+import { Head } from '@inertiajs/inertia-vue3'
+
+export default {
+  components: { Head },
+  props: {
+    title: String
+  }
+}
+</script>
+<style lang="">
+  
+</style>
