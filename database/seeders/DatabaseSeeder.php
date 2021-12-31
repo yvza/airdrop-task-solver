6a7c709fbf6d284b6d95c0e2ee1a9d2fdd8d7a73
yuza <akhmadyuza@gmail.com>
2021-12-31 22:59:19 +0700

    update list seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            ListKataKataSeeder::class
+            ListKataKataSeeder::class,
+            TaskNameListSeeder::class,
+            TwitterTaskSeeder::class,
         ]);
     }
 }
