c3c9b8c5ca881002ba531d796cd0d8d5a003fbf5
yuza <akhmadyuza@gmail.com>
2021-12-31 22:51:12 +0700

    defining table and guarded field

---
+++ app/Models/TaskResults.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TaskResults extends Model
+{
+    use HasFactory;
+    protected $table = 'task_result';
+    protected $guarded = ['id'];
+}
