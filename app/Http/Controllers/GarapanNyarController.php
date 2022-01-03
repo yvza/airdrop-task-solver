cc2114f99fdfc8b9d17ce5a0519806daf45e437d
yuza <akhmadyuza@gmail.com>
2022-01-03 15:37:20 +0700

    core follow functionality

--- app/Http/Controllers/GarapanNyarController.php
+++ app/Http/Controllers/GarapanNyarController.php
@@ -3,7 +3,13 @@
 namespace App\Http\Controllers;
 
 use Inertia\Inertia;
+use App\Models\Tasks;
+use App\Models\Garapan;
+use App\Models\TaskResults;
 use Illuminate\Http\Request;
+use League\OAuth1\Client\Server\Twitter;
+use Coderjerk\BirdElephant\BirdElephant;
+use Symfony\Component\Console\Input\Input;
 
 class GarapanNyarController extends Controller
 {
@@ -35,7 +41,7 @@ class GarapanNyarController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
+        $validated = $request->validate([
             'airdrop_name' => ['required'],
             'project_url' => ['required'],
             'distribution_date' => ['required'],
@@ -43,7 +49,79 @@ class GarapanNyarController extends Controller
             'target_url' => ['required']
         ]);
 
-        return redirect('/');
+        // validate input
+        if (!$validated) {
+            return redirect('/');
+        }
+
+        // check when airdrop_name exist
+        $nameExist = Garapan::where('airdrop_name', '=', $request->airdrop_name)->first();
+        if ($nameExist) {
+            // get id
+            $garapan_id = $nameExist->id;
+
+            // TODO: make validation before create new Tasks
+            // next, save to tasks
+            $tasks = Tasks::create([
+                'garapan_id' => $garapan_id,
+                'task_name_list_id' => $request->task_name,
+            ]);
+
+            // decoded url, get tweet username
+            $decoded_target_username = array_filter(explode("/", parse_url($request->target_url)['path']))[1];
+            if (!$decoded_target_username) {
+                return redirect('/');
+            }
+            
+            // authorization bearer
+            $twitter_authorization_bearer = env('TWITTER_AUTHORIZATION_BEARER');
+            
+            // some config
+            $twitter_api_key = env('TWITTER_API_KEY');
+            $twitter_api_key_secret = env('TWITTER_API_KEY_SECRET');
+            $twitter_client_id = env('TWITTER_CLIENT_ID');
+            $twitter_client_secret = env('TWITTER_CLIENT_SECRET');
+            $twitter_user_id = env('TWITTER_USER_ID');
+            
+            // https://developer.twitter.com/en/docs/tutorials/authenticating-with-twitter-api-for-enterprise/oauth1-0a-and-user-access-tokens
+            // https://developer.twitter.com/en/docs/authentication/oauth-1-0a/obtaining-user-access-tokens
+            // follwo action
+            $credentials = array(
+                'bearer_token' => $twitter_authorization_bearer, // OAuth 2.0 Bearer Token requests
+                'consumer_key' => $twitter_api_key, // identifies your app, always needed
+                'consumer_secret' => $twitter_api_key_secret, // app secret, always needed
+                'token_identifier' => '523159523-do7jsdjJ4UNCPeGxkAZzEmSRMH6XOkV6JNmXCP55', // OAuth 1.0a User Context requests
+                'token_secret' => 'FeWlyZ6gUTxrxImwpoMDCoe0CDBQnkoHm1fQPsvk2wUqk', // OAuth 1.0a User Context requests
+            );
+            
+            $twitter = new BirdElephant($credentials);
+            $user = $twitter->user(env('TWITTER_USERNAME'));
+            $follow = $user->follow($decoded_target_username);
+            if ($follow->data->following) {
+                return redirect()->back()->with('twitter_follow', 1);
+            } else {
+                return redirect()->back()->with('twitter_follow', 0);
+            }
+            // return redirect()->back()->with();
+
+            // next, do the task, end then save the result
+            // $task_result = new TaskResults;
+            // $task_result->tasks_id = $tasks['id'];
+            // $task_result->task_type_id = $request->type;
+            // $task_result->status = $request->type;
+            // $task_result->save();
+        } else {
+            // save
+            $garapan = Garapan::create([
+                'project_url' => $request->project_url, 
+                'distribution_date' => $request->distribution_date, 
+                'airdrop_name' => $request->airdrop_name, 
+            ]);
+
+            dd($garapan);
+        }
+
+        // return redirect('/');
     }
 
     /**
