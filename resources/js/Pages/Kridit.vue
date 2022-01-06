f0f75b1814b9aed56c1242cae74c36b0755eddad
yuza <akhmadyuza@gmail.com>
2022-01-06 13:29:10 +0700

    base structure

--- resources/js/Pages/Kridit.vue
+++ resources/js/Pages/Kridit.vue
@@ -1,13 +1,22 @@
 <template lang="">
+  <AppHead title="Kridit">
+    <meta name="description" content="Kridit - Airdrop Task Solver" />
+  </AppHead>
   <Layout>
-    Kridit
+    <article class="w-full flex flex-col">
+      Kridit
+    </article>
   </Layout>
 </template>
 <script>
 import Layout from '../Shared/Layout.vue'
+import AppHead from '../Shared/AppHead.vue'
 
 export default {
-  components: { Layout }
+  components: { 
+    Layout,
+    AppHead 
+  }
 }
 </script>
 <style lang="">
