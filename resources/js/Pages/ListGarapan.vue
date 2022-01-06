909f1154c236b15a33eda22c5d9af3be05d7d59e
yuza <akhmadyuza@gmail.com>
2022-01-06 17:12:42 +0700

    base core list garapan

--- resources/js/Pages/ListGarapan.vue
+++ resources/js/Pages/ListGarapan.vue
@@ -12,77 +12,56 @@
       </div>
       <div class="content grow bg-slate-100 p-10 flex flex-row">
         <div class="flex-grow mr-5 h-full">
-          <p class="text-center text-2xl mb-9">Land Saiki (3)</p>
+          <p v-if="!today" class="text-center text-2xl mb-9">Land Saiki ({{ list_garapan[0].length }})</p>
+          <p v-if="today" class="text-center text-2xl mb-9">Land Saiki (-)</p>
           <div class="max-h-[33rem] overflow-y-scroll">
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
-              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
-              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            <div v-for="garapan in list_garapan[0]" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">{{ garapan.airdrop_name }}</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" :href="garapan.project_url">{{ garapan.project_url }}</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>{{ garapan.distribution_date }}</p>
+              <p><i class="far fa-dot-circle mr-2"></i>{{ garapan.created_at }}</p>
             </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
-            </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
-            </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
-            </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
+            <div v-if="today" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Empty</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="#">-</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>-</p>
+              <p><i class="far fa-dot-circle mr-2"></i>-</p>
             </div>
           </div>
         </div>
         <div class="flex-grow mr-5 h-full">
-          <p class="text-center text-2xl mb-9">Telong dino neh (7)</p>
+          <p v-if="!nextThreeDays" class="text-center text-2xl mb-9">Telong dino neh ({{ list_garapan[1].length }})</p>
+          <p v-if="nextThreeDays" class="text-center text-2xl mb-9">Telong dino neh (-)</p>
           <div class="max-h-[33rem] overflow-y-scroll">
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
-              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
-              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            <div v-for="garapan in list_garapan[1]" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">{{ garapan.airdrop_name }}</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" :href="garapan.project_url">{{ garapan.project_url }}</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>{{ garapan.distribution_date }}</p>
+              <p><i class="far fa-dot-circle mr-2"></i>{{ garapan.created_at }}</p>
             </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
-            </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
+            <div v-if="nextThreeDays" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Empty</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="#">-</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>-</p>
+              <p><i class="far fa-dot-circle mr-2"></i>-</p>
             </div>
           </div>
         </div>
         <div class="flex-grow h-full">
-          <p class="text-center text-2xl mb-9">Jek Suwe (11)</p>
+          <p v-if="!stillInLongTime" class="text-center text-2xl mb-9">Jek Suwe ({{ list_garapan[2].length }})</p>
+          <p v-if="stillInLongTime" class="text-center text-2xl mb-9">Jek Suwe (-)</p>
           <div class="max-h-[33rem] overflow-y-scroll">
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
-              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
-              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            <div v-for="garapan in list_garapan[2]" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">{{ garapan.airdrop_name }}</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" :href="garapan.project_url">{{ garapan.project_url }}</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>{{ garapan.distribution_date }}</p>
+              <p><i class="far fa-dot-circle mr-2"></i>{{ garapan.created_at }}</p>
             </div>
-            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
-              <p class="font-bold">Astroport</p>
-              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
-              <p>6 Desember 2022</p>
-              <p>1 Oktober 2022</p>
+            <div v-if="stillInLongTime" class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Empty</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="#">-</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>-</p>
+              <p><i class="far fa-dot-circle mr-2"></i>-</p>
             </div>
           </div>
         </div>
@@ -91,14 +70,27 @@
   </Layout>
 </template>
 <script>
+import _ from 'lodash'
 import Layout from '../Shared/Layout.vue'
 import AppHead from '../Shared/AppHead.vue'
 
 export default {
+  props: ['list_garapan'],
   components: { 
     Layout,
     AppHead 
-  }
+  },
+  computed: {
+    today() {
+      return _.isEmpty(this.list_garapan[0])
+    },
+    nextThreeDays() {
+      return _.isEmpty(this.list_garapan[1])
+    },
+    stillInLongTime() {
+      return _.isEmpty(this.list_garapan[2])
+    }
+  },
 }
 </script>
 <style lang="">
