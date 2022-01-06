68a5a8fc0ccc37433d0281e65644e198c381f020
yuza <akhmadyuza@gmail.com>
2022-01-06 22:15:52 +0700

    base update

--- resources/js/Pages/Kridit.vue
+++ resources/js/Pages/Kridit.vue
@@ -4,7 +4,15 @@
   </AppHead>
   <Layout>
     <article class="w-full flex flex-col">
-      Kridit
+      <div class="px-10 p-10 shadow-lg">
+        <div class="head-content">
+          <h1 class="text-2xl font-bold font-sans">Kridit</h1>
+          <h6 class="text-sm text-slate-400 font-sans dark:text-slate-900">Shottss gawe wong-wong seng melu kontribusi (Contributor and Collaborator)</h6>
+        </div>
+      </div>
+      <div class="content grow bg-slate-100 p-10 flex flex-row">
+        
+      </div>
     </article>
   </Layout>
 </template>
