6489a56d8bb56065d5742ee01562043c532272b4
yuza <akhmadyuza@gmail.com>
2022-01-06 12:03:41 +0700

    a follow, love, retweet, quote tweet function

--- app/Http/Controllers/GarapanNyarController.php
+++ app/Http/Controllers/GarapanNyarController.php
@@ -6,10 +6,12 @@ use Inertia\Inertia;
 use App\Models\Tasks;
 use App\Models\Garapan;
 use App\Models\TaskResults;
+use App\Models\ListKataKata;
 use App\Models\TwitterToken;
 use Illuminate\Http\Request;
-use League\OAuth1\Client\Server\Twitter;
+use Illuminate\Support\Facades\DB;
 use Coderjerk\BirdElephant\BirdElephant;
+use Coderjerk\BirdElephant\Compose\Tweet;
 use Symfony\Component\Console\Input\Input;
 
 class GarapanNyarController extends Controller
@@ -54,33 +56,53 @@ class GarapanNyarController extends Controller
         if (!$validated) {
             return redirect('/');
         }
-
+        
         // check when airdrop_name exist
         $nameExist = Garapan::where('airdrop_name', '=', $request->airdrop_name)->first();
         if ($nameExist) {
-            // get id
-            $garapan_id = $nameExist->id;
+            // validate if choosen task already done
+            $taskAlreadyDone = DB::table('tasks')
+            ->join('garapan', function ($join) use ($nameExist) {
+                $join->on('tasks.garapan_id', '=', 'garapan.id')
+                ->where('garapan.airdrop_name', '=', $nameExist->airdrop_name); //automatic check from exiting
+            })
+            ->join('task_results', function ($join) use ($request) {
+                $join->on('tasks.id', '=', 'tasks_id')
+                ->where('task_results.task_type_id', '=', $request->type) //depends on task type, eg: follow = 1
+                ->where('task_results.status', '=', 1); //querying success result
+            })
+            ->where('task_name_list_id', '=', 1) //depends on task, eg: twitter = 1
+            ->count();
 
-            // TODO: make validation before create new Tasks
-            // next, save to tasks
-            $tasks = Tasks::create([
-                'garapan_id' => $garapan_id,
-                'task_name_list_id' => $request->task_name,
-            ]);
+            if ($taskAlreadyDone > 0) {
+                // return with quote tweet id when already done, only for quote tweet task
+                if ((int)$request->type === 4) {
+                    $returnUrl = DB::table('tasks')
+                    ->join('garapan', function ($join) use ($nameExist) {
+                        $join->on('tasks.garapan_id', '=', 'garapan.id')
+                        ->where('garapan.airdrop_name', '=', $nameExist->airdrop_name); //automatic check from exiting
+                    })
+                    ->join('task_results', function ($join) use ($request) {
+                        $join->on('tasks.id', '=', 'tasks_id')
+                        ->where('task_results.task_type_id', '=', $request->type) //depends on task type, eg: follow = 1
+                        ->where('task_results.status', '=', 1); //querying success result
+                    })
+                    ->where('task_name_list_id', '=', 1) //depends on task, eg: twitter = 1
+                    ->get();
 
-            // decoded url, get tweet username
-            $decoded_target_username = array_filter(explode("/", parse_url($request->target_url)['path']))[1];
-            if (!$decoded_target_username) {
-                return redirect('/');
+                    $url_result = 'https://twitter.com/'.env('TWITTER_USERNAME').'/status/'.$returnUrl[0]->additional_message;
+
+                    return redirect()->back()->with('error_message', 'Already Done! 😃😄 '.$url_result);
+                }
+
+                return redirect()->back()->with('error_message', 'Already Done! 😃😄');
             }
-            
+
             // authorization bearer
             $twitter_authorization_bearer = env('TWITTER_AUTHORIZATION_BEARER');
-            
             // some config
             $twitter_api_key = env('TWITTER_API_KEY');
             $twitter_api_key_secret = env('TWITTER_API_KEY_SECRET');
-            
             // https://developer.twitter.com/en/docs/tutorials/authenticating-with-twitter-api-for-enterprise/oauth1-0a-and-user-access-tokens
             // https://developer.twitter.com/en/docs/authentication/oauth-1-0a/obtaining-user-access-tokens
             // follwo action
@@ -95,18 +117,128 @@ class GarapanNyarController extends Controller
                 'token_identifier' => $twitter_token[0]['oauth_token'], // OAuth 1.0a User Context requests
                 'token_secret' => $twitter_token[0]['oauth_token_secret'], // OAuth 1.0a User Context requests
             );
-            
-            $twitter = new BirdElephant($credentials);
-            $user = $twitter->user(env('TWITTER_USERNAME'));
-            $follow = $user->follow($decoded_target_username);
-            if ($follow->data->following) return redirect()->back()->with('success_message', 'Berhasil Difollow! 😄');
 
-            // next, do the task, end then save the result
-            // $task_result = new TaskResults;
-            // $task_result->tasks_id = $tasks['id'];
-            // $task_result->task_type_id = $request->type;
-            // $task_result->status = $request->type;
-            // $task_result->save();
+            // get id
+            $garapan_id = $nameExist->id;
+            // decoded url
+            $decoded_url = array_filter(explode("/", parse_url($request->target_url)['path']));
+            // twitter instace
+            $twitter = new BirdElephant($credentials);
+            $weDo = $twitter->user(env('TWITTER_USERNAME'));
+
+            switch ($request->type) {
+                case '1':
+                    # follow
+                    // decoded url, get tweet username
+                    $target_username = $decoded_url[1];
+                    if (!$target_username) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $follow = $weDo->follow($target_username);
+
+                    if ($follow->data->following) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan_id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Difollow! 😄');
+                    }
+                    break;
+                case '2':
+                    # love
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $like = $weDo->like($target_url);
+                    if ($like->data->liked) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan_id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Like! 😄');
+                    }
+                    break;
+                case '3':
+                    # retweet
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $retweet = $weDo->retweet($target_url);
+                    if ($retweet->data->retweeted) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan_id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Retweett! 😄');
+                    }
+                    break;
+                case '4':
+                    # quote tweet
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // pick random kata kata
+                    $kata_kata = ListKataKata::inRandomOrder()->first();
+                    // next, do the task, end then save the result
+                    $tweet = (new Tweet)->text($kata_kata->message)->quoteTweetId($target_url);
+                    $quote_tweet = $twitter->tweets()->tweet($tweet);
+                    if ($quote_tweet->data->id) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan_id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->additional_message = $quote_tweet->data->id; //saving id tweet from callback
+                        $task_result->save();
+
+                        $url_result = 'https://twitter.com/'.env('TWITTER_USERNAME').'/status/'.$quote_tweet->data->id;
+
+                        return redirect()->back()->with('success_message', 'Berhasil Quote Retweett! 😄 '.$url_result);
+                    }
+                    break;
+            }
         } else {
             // save
             $garapan = Garapan::create([
@@ -115,10 +247,146 @@ class GarapanNyarController extends Controller
                 'airdrop_name' => $request->airdrop_name, 
             ]);
 
-            dd($garapan);
-        }
+            // authorization bearer
+            $twitter_authorization_bearer = env('TWITTER_AUTHORIZATION_BEARER');
+            // some config
+            $twitter_api_key = env('TWITTER_API_KEY');
+            $twitter_api_key_secret = env('TWITTER_API_KEY_SECRET');
+            // https://developer.twitter.com/en/docs/tutorials/authenticating-with-twitter-api-for-enterprise/oauth1-0a-and-user-access-tokens
+            // https://developer.twitter.com/en/docs/authentication/oauth-1-0a/obtaining-user-access-tokens
+            // follwo action
+            $twitter_token = TwitterToken::all()->toArray();
+            if (empty($twitter_token)) {
+                return redirect()->back()->with('twitter_token', 'Akses dulu http://airdrop-task-solver.test/twitter-token untuk dapetin tokennya 😋');
+            }
+            $credentials = array(
+                'bearer_token' => $twitter_authorization_bearer, // OAuth 2.0 Bearer Token requests
+                'consumer_key' => $twitter_api_key, // identifies your app, always needed
+                'consumer_secret' => $twitter_api_key_secret, // app secret, always needed
+                'token_identifier' => $twitter_token[0]['oauth_token'], // OAuth 1.0a User Context requests
+                'token_secret' => $twitter_token[0]['oauth_token_secret'], // OAuth 1.0a User Context requests
+            );
 
-        // return redirect('/');
+            // decoded url
+            $decoded_url = array_filter(explode("/", parse_url($request->target_url)['path']));
+            // twitter instace
+            $twitter = new BirdElephant($credentials);
+            $weDo = $twitter->user(env('TWITTER_USERNAME'));
+
+            switch ($request->type) {
+                case '1':
+                    # follow
+                    // decoded url, get tweet username
+                    $target_username = $decoded_url[1];
+                    if (!$target_username) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $follow = $weDo->follow($target_username);
+
+                    if ($follow->data->following) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan->id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Difollow! 😄');
+                    }
+                    break;
+                case '2':
+                    # love
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $like = $weDo->like($target_url);
+                    if ($like->data->liked) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan->id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Like! 😄');
+                    }
+                    break;
+                case '3':
+                    # retweet
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // next, do the task, end then save the result
+                    $retweet = $weDo->retweet($target_url);
+                    if ($retweet->data->retweeted) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan->id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->save();
+
+                        return redirect()->back()->with('success_message', 'Berhasil Retweett! 😄');
+                    }
+                    break;
+                case '4':
+                    # quote tweet
+                    // decoded url, get tweet id
+                    $target_url = $decoded_url[3];
+                    if (!$target_url) {
+                        return redirect('/');
+                    }
+
+                    // pick random kata kata
+                    $kata_kata = ListKataKata::inRandomOrder()->first();
+                    // next, do the task, end then save the result
+                    $tweet = (new Tweet)->text($kata_kata->message)->quoteTweetId($target_url);
+                    $quote_tweet = $twitter->tweets()->tweet($tweet);
+                    if ($quote_tweet->data->id) {
+                        // next, save to tasks
+                        $tasks = Tasks::create([
+                            'garapan_id' => $garapan->id,
+                            'task_name_list_id' => $request->task_name,
+                        ]);
+
+                        $task_result = new TaskResults;
+                        $task_result->tasks_id = $tasks['id'];
+                        $task_result->task_type_id = $request->type;
+                        $task_result->status = 1;
+                        $task_result->additional_message = $quote_tweet->data->id; //saving id tweet from callback
+                        $task_result->save();
+
+                        $url_result = 'https://twitter.com/'.env('TWITTER_USERNAME').'/status/'.$quote_tweet->data->id;
+
+                        return redirect()->back()->with('success_message', 'Berhasil Quote Retweett! 😄 '.$url_result);
+                    }
+                    break;
+            }
+        }
     }
 
     /**
