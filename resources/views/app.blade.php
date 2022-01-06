8eb8591e557667de4d15ed14361d5fe263787deb
yuza <akhmadyuza@gmail.com>
2022-01-06 20:11:11 +0700

    adding google analytics tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,15 @@
     <link rel="icon" href="{{ asset('images/favico_airdrop_task_solver.png') }}">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-SXQQSBHQYM"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-SXQQSBHQYM');
+    </script>
   </head>
   <body>
     @inertia
