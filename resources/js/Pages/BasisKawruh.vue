24cf60cac692347bdb1d56a08a724fd57daca87b
yuza <akhmadyuza@gmail.com>
2022-01-06 22:15:26 +0700

    konten added

--- resources/js/Pages/BasisKawruh.vue
+++ resources/js/Pages/BasisKawruh.vue
@@ -4,7 +4,52 @@
   </AppHead>
   <Layout>
     <article class="w-full flex flex-col">
-      Basis Kawruh
+      <div class="px-10 p-10 shadow-lg">
+        <div class="head-content">
+          <h1 class="text-2xl font-bold font-sans">Basis Kawruh</h1>
+          <h6 class="text-sm text-slate-400 font-sans dark:text-slate-900">Iki ngunu ceritane kenopo aplikasi ini digawe</h6>
+        </div>
+      </div>
+      <div class="content grow bg-slate-100 p-10 flex flex-row">
+        <p class="max-h-[37rem] overflow-y-scroll w-full">
+          Sudah menjadi keinginan sejak lama, bahkan sejak awal mulai menekuni dunia Web Development. <br><br>
+          
+          Aku inget betul kata Mas Hilman (Sekolah Koding), saat pertama kali Playlist Tutorial PHP Fundamental di channel YTnya baru dibangun. <br>
+          Salah satu pendengar setia Teh Atau Kopi, sejak awal episode hingga saat ini. <br>
+          Ada kata yang menarik dari salah satu episodenya. <br><br>
+  
+          "Ide Itu Murah Eksekusi Adalah Segalanya". <br><br>
+  
+          Disitulah aku mulai berfikir, kapan ya aku bisa bermanfaat bagi orang lain. <br>
+          Dan tentunya melalui skill yang kumiliki sekarang. <br>
+          2016 adalah awal diriku mempelajari bahasa pemrograman PHP. <br>
+          Disaat itu juga perjalananku di Dunia Maya dimulai, sempat masuk ke Cyber Crime sebagai player, dari Deface sampai Carding. <br>
+          Dan mendokumentasikan semua eksperimenku ke blog. <br>
+          Sampai akhirnya dari dua pilihan Web Pentester dan Web Developer, aku memutuskan untuk masuk sebagai Web Developer. <br>
+          Jejak digital akan selalu ada, dan sengaja tak biarin, i have a Pride on that. <br><br>
+
+          Singkat cerita banyak hal yang kulalui sebagai Web dan Mobile Dev. <br>
+          Banyak ide yang saat itu gagal kueksekusi karena terlalu banyak hal yang harus dilakukan, khususnya effort ke waktu yang harus tinggi. <br><br>
+
+          Sharing Gils Blog adalah salah satu tempat tersolid untuk berbagi berbagai macam intrumen di kehidupan ini. <br>
+          Gw lihat-lihat minat terbesar disana didominasi oleh orang-orang yang ingin dapet cuan dalam waktu singkat. <br>
+          "Airdrop" adalah salah satu item yang sangat diminati di group itu. <br>
+          Sampai terlintas di pikiranku, "Apa aku harus bikin ya, aplikasi gitu yang buat paling nggk mempersingkat step yang harus dilakuin!". <br>
+          "Apa aku bisa ya", "Apa apliksinya ada yang make ya". <br><br>
+
+          Momen itu muncul lagi, dimana aku punya suatu Ide, dan saat tulisan ini ditulis, belum ada yang buat aplikasi sekompleks ini. <br>
+          Oke! kali ini gak akan gw sia-sia-in lagi, akan gw eksekusi! <br>
+          Ditengah-tengah aktifitasku aku meluangkan sedikit waktu untuk riset dan merancang aplikasi ini. <br>
+          Kumulai Awal Desember 2021. <br>
+          Ngasih progress dikit-dikit, dari pada nggak sama sekali. <br>
+
+          Aku butuh bantuan kalian semua. <br>
+          Kasih gw Support dan Feedback. <br>
+          Apapun itu akan sangat gw Apresiasi! <br><br>
+          
+          - Yuza <br><br>
+        </p>
+      </div>
     </article>
   </Layout>
 </template>
