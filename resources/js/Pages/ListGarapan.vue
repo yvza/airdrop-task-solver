934f8339afef6b9fc14e30c36ad9923e6e0622fa
yuza <akhmadyuza@gmail.com>
2022-01-06 14:41:37 +0700

    base code list garapan

--- resources/js/Pages/ListGarapan.vue
+++ resources/js/Pages/ListGarapan.vue
@@ -4,7 +4,89 @@
   </AppHead>
   <Layout>
     <article class="w-full flex flex-col">
-      List Garapan
+      <div class="px-10 p-10 shadow-lg">
+        <div class="head-content">
+          <h1 class="text-2xl font-bold font-sans">List Garapan</h1>
+          <h6 class="text-sm text-slate-400 font-sans dark:text-slate-900">Iki gawe sorting garapan seng kate land</h6>
+        </div>
+      </div>
+      <div class="content grow bg-slate-100 p-10 flex flex-row">
+        <div class="flex-grow mr-5 h-full">
+          <p class="text-center text-2xl mb-9">Land Saiki (3)</p>
+          <div class="max-h-[33rem] overflow-y-scroll">
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
+              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex-grow mr-5 h-full">
+          <p class="text-center text-2xl mb-9">Telong dino neh (7)</p>
+          <div class="max-h-[33rem] overflow-y-scroll">
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
+              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+          </div>
+        </div>
+        <div class="flex-grow mr-5 h-full">
+          <p class="text-center text-2xl mb-9">Jek Suwe (11)</p>
+          <div class="max-h-[33rem] overflow-y-scroll">
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p><i class="fas fa-link mr-2"></i><a class="text-sky-900" target="_blank" href="https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96">https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</a></p>
+              <p><i class="fas fa-calendar-day mr-2"></i>6 Desember 2022</p>
+              <p><i class="far fa-dot-circle mr-2"></i>1 Oktober 2022</p>
+            </div>
+            <div class="rounded-md border-solid border-2 border-sky-900 p-4 my-3">
+              <p class="font-bold">Astroport</p>
+              <p>https://astroport.medium.com/unveiling-the-great-tefi-chad-astro-airdrop-3ff437f43f96</p>
+              <p>6 Desember 2022</p>
+              <p>1 Oktober 2022</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </article>
   </Layout>
 </template>
