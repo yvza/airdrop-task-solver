2ee1efe44f5c47ed58d2b9eb651c564537c255b2
yuza <akhmadyuza@gmail.com>
2022-01-06 13:30:03 +0700

    base structure

--- resources/js/Pages/NgaturKataKata.vue
+++ resources/js/Pages/NgaturKataKata.vue
@@ -1,13 +1,22 @@
 <template lang="">
+  <AppHead title="Ngatur Kata Kata">
+    <meta name="description" content="Ngatur Kata Kata - Airdrop Task Solver" />
+  </AppHead>
   <Layout>
-    Ngatur Kata Kata
+    <article class="w-full flex flex-col">
+      Soon!
+    </article>
   </Layout>
 </template>
 <script>
 import Layout from '../Shared/Layout.vue'
+import AppHead from '../Shared/AppHead.vue'
 
 export default {
-  components: { Layout }
+  components: { 
+    Layout,
+    AppHead 
+  }
 }
 </script>
 <style lang="">
