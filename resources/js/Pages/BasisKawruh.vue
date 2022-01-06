0e97b34737b18c913170f2dd0b15d8317ad88fff
yuza <akhmadyuza@gmail.com>
2022-01-06 13:28:47 +0700

    base structure

--- resources/js/Pages/BasisKawruh.vue
+++ resources/js/Pages/BasisKawruh.vue
@@ -1,13 +1,22 @@
 <template lang="">
+  <AppHead title="Basis Kawruh">
+    <meta name="description" content="Basis Kawruh - Airdrop Task Solver" />
+  </AppHead>
   <Layout>
-    Basis Kawruh
+    <article class="w-full flex flex-col">
+      Basis Kawruh
+    </article>
   </Layout>
 </template>
 <script>
 import Layout from '../Shared/Layout.vue'
+import AppHead from '../Shared/AppHead.vue'
 
 export default {
-  components: { Layout }
+  components: { 
+    Layout,
+    AppHead 
+  }
 }
 </script>
 <style lang="">
